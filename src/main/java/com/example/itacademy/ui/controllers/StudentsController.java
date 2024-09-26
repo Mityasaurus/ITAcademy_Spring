7615df130549c5c036c293e56f5d3fc3289e303d
Mityasaurus <dmitriybovdun5@gmail.com>
2024-09-26 20:56:13 +0300

    add students form and table with thymeleaf

--- src/main/java/com/example/itacademy/ui/controllers/StudentsController.java
+++ src/main/java/com/example/itacademy/ui/controllers/StudentsController.java
@@ -1,20 +1,32 @@
 package com.example.itacademy.ui.controllers;
 
+import com.example.itacademy.data.services.StudentService;
+import com.example.itacademy.models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class StudentsController {
+    @Autowired
+    private StudentService studentService;
+
     @GetMapping("students")
-    public String load(){
+    public String load(Model model){
+        List<Student> list = studentService.findAll();
+        model.addAttribute("studentList", list);
         return "students";
     }
 
     @PostMapping("addStudentForm")
-    public String addStudentForm(@RequestParam("name") String name){
-        System.err.println(name);
+    public String addStudentForm(@ModelAttribute("Student") Student student){
+        studentService.save(student);
         return "redirect:students";
     }
 }
