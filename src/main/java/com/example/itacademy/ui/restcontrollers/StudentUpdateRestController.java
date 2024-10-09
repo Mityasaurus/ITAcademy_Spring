9bca188ab6fba8dcd0c920b26fea837696f7cfe7
Mityasaurus <dmitriybovdun5@gmail.com>
2024-10-09 12:46:21 +0300

    add student payment update controller method and fix script

--- src/main/java/com/example/itacademy/ui/restcontrollers/StudentUpdateRestController.java
+++ src/main/java/com/example/itacademy/ui/restcontrollers/StudentUpdateRestController.java
@@ -1,13 +1,18 @@
 package com.example.itacademy.ui.restcontrollers;
 
 import com.example.itacademy.data.services.PaymentService;
+import com.example.itacademy.data.services.StudentService;
 import com.example.itacademy.models.Payment;
+import com.example.itacademy.models.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class StudentUpdateRestController {
@@ -15,7 +20,27 @@ public class StudentUpdateRestController {
     @Autowired
     PaymentService paymentService;
 
+    @Autowired
+    StudentService studentService;
+
     @PostMapping("/rest/paymentUpdateForm")
+    public ResponseEntity<?> paymentUpdateForm(@RequestParam("studentId") Integer studentId, @RequestBody Payment payment){
+//        System.err.println(payment);
+//        System.err.println(studentId);
+        try {
+            Optional<Student> student = studentService.findById(studentId);
+            if(student.isPresent()){
+                payment.setStudent(student.get());
+                paymentService.update(payment);
+            }
+            else{
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            }
             return new ResponseEntity<>(HttpStatus.OK);
         }
+        catch(Exception ex){
+            System.err.println(ex.getMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/resources/static/js/studentUpdate.js
+++ src/main/resources/static/js/studentUpdate.js
@@ -1,7 +1,29 @@
-document
-  .querySelectorAll("#btnPaymentSave")
-  .forEach((btn) => btn.addEventListener("click", updatePayment));
+const forms = document.querySelectorAll("#payments-table-container form");
 
-function updatePayment() {
-  console.log("updatePayment");
+forms.forEach(function (form) {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    updatePayment(form);
+  });
+});
+
+function updatePayment(form) {
+  let formData = new FormData(form);
+  let studentId = formData.get("studentId");
+  let json = JSON.stringify(Object.fromEntries(formData));
+  // console.log(json);
+  //
+  fetch(`/rest/paymentUpdateForm?studentId=${studentId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: json,
+  })
+    .then((response) => {
+      // console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
